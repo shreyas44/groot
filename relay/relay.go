2e6750837e8a2b562247dea084105c811989aeef
shreyas44 <46835608+shreyas44@users.noreply.github.com>
2021-10-18 14:08:16 +0000

    Fix problems with args validation Create NodeDefinition constructor

--- relay/relay.go
+++ relay/relay.go
@@ -1,9 +1,20 @@
 package relay
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/shreyas44/groot"
+)
 
 type NodeDefinition struct {
-	ID string `json:"id"`
+	groot.InterfaceType
+	ID groot.StringID `json:"id"`
+}
+
+func NewNodeDefinition(id string) NodeDefinition {
+	return NodeDefinition{
+		ID: groot.StringID(id),
+	}
 }
 
 type Node interface {
@@ -22,30 +33,34 @@ type PageInfo struct {
 }
 
 type PaginationArgs struct {
-	First  int    `json:"first"`
-	Last   int    `json:"last"`
-	After  string `json:"after"`
-	Before string `json:"before"`
+	First  *int    `json:"first"`
+	Last   *int    `json:"last"`
+	After  *string `json:"after"`
+	Before *string `json:"before"`
 }
 
 func (args PaginationArgs) Validate() error {
-	if args.First != 0 && args.Last != 0 {
+	if args.First == nil && args.Last == nil {
+		return errors.New("first or last must be specified")
+	}
+
+	if args.First != nil && args.Last != nil {
 		return errors.New("first and last cannot be set at the same time")
 	}
 
-	if args.First < 0 || args.Last < 0 {
+	if *args.First < 0 || *args.Last < 0 {
 		return errors.New("first and last must be greater than 0")
 	}
 
-	if args.After != "" && args.Before != "" {
+	if args.After != nil && args.Before != nil {
 		return errors.New("after and before cannot be set at the same time")
 	}
 
-	if args.First != 0 && args.Before != "" {
+	if args.First != nil && args.Before != nil {
 		return errors.New("first and before cannot be set at the same time")
 	}
 
-	if args.Last != 0 && args.After != "" {
+	if args.Last != nil && args.After != nil {
 		return errors.New("last and after cannot be set at the same time")
 	}
 
